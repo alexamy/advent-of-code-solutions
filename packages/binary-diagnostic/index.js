1e14b7a10af976f8ee042b6e31d81794caa1c5d1
Alexander Chaschin <ac@happynumbers.com>
2022-01-30 18:06:56 +0300

    copy package

---
+++ packages/binary-diagnostic/index.js
@@ -0,0 +1,5 @@
+const solve1 = () => {};
+
+const solve2 = () => {};
+
+export { solve1, solve2 };
---
+++ packages/binary-diagnostic/run.js
@@ -0,0 +1,8 @@
+import { solve1, solve2 } from './index.js';
+
+const result1 = solve1();
+
+const result2 = solve2();
+
+const messages = [`Part 1: ${result1}`, `Part 2: ${result2}`];
+console.log(messages.join('\n'));
