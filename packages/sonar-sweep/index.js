56dff54da9121da0b23e2d4c759fbb2bfbb43a54
Alexander Chaschin <ac@happynumbers.com>
2022-01-30 13:33:34 +0300

    group into methods

--- packages/sonar-sweep/index.js
+++ packages/sonar-sweep/index.js
@@ -7,33 +7,43 @@ const DEC = 'dec';
 const measurementsBuffer = await fs.promises.readFile('input.txt');
 const measurements = measurementsBuffer.toString().split('\n').map(Number);
 
-// count directions (part 1)
-const trendsByOne = [];
-for(let i = 0; i < measurements.length - 1; i++) {
-  const current = measurements[i];
-  const next = measurements[i+1];
+const solvePart1 = () => {
+  // count directions
+  const trends = [];
+  for(let i = 0; i < measurements.length - 1; i++) {
+    const current = measurements[i];
+    const next = measurements[i+1];
 
-  trendsByOne.push(next > current ? INC : DEC);
+    trends.push(next > current ? INC : DEC);
+  }
+
+  // find result count
+  const result = trends.filter(direction => direction === INC).length;
+
+  return result;
 }
 
-// count directions (part 2)
-const sumsByThree = [];
-for(let i = 0; i < measurements.length - 3; i++) {
-  const [w1, w2, w3] = measurements.slice(i, i + 3);
-  sumsByThree.push(w1 + w2 + w3);
+const solvePart2 = () => {
+  // count directions
+  const sums = [];
+  for(let i = 0; i < measurements.length - 3; i++) {
+    const [w1, w2, w3] = measurements.slice(i, i + 3);
+    sums.push(w1 + w2 + w3);
+  }
+
+  const trends = [];
+  for(let i = 0; i < sums.length - 1; i++) {
+    const current = sums[i];
+    const next = sums[i+1];
+
+    trends.push(next > current ? INC : DEC);
+  }
+
+  // find result count
+  const result = trends.filter(direction => direction === INC).length;
+
+  return result;
 }
 
-const trendsByThree = [];
-for(let i = 0; i < sumsByThree.length - 1; i++) {
-  const current = sumsByThree[i];
-  const next = sumsByThree[i+1];
-
-  trendsByThree.push(next > current ? INC : DEC);
-}
-
-// find result count
-const resultPart1 = trendsByOne.filter(direction => direction === INC).length;
-console.log(`Part 1: ${resultPart1}`);
-
-const resultPart2 = trendsByThree.filter(direction => direction === INC).length;
-console.log(`Part 2: ${resultPart2}`);
+console.log(`Part 1: ${solvePart1()}`);
+console.log(`Part 2: ${solvePart2()}`);
