7eb2d63e24823738ab2aa9fcae5ca0d2982a2363
Alexander Chaschin <ac@happynumbers.com>
2022-01-30 20:08:00 +0300

    extract helpers

--- packages/binary-diagnostic/index.js
+++ packages/binary-diagnostic/index.js
@@ -7,9 +7,7 @@ const readInput = async () => {
   return input;
 };
 
-const solve1 = (string) => {
-  const numbers = string.trim().split('\n');
-
+const findMostCommonDigits = (numbers) => {
   const groups = numbers.reduce((groups, number) => {
     number.split('').forEach((digit, i) => {
       if(!groups[i]) groups[i] = [];
@@ -24,17 +22,36 @@ const solve1 = (string) => {
     .map(group => _.countBy(group, _.identity))
     .map(c => c['0'] > c['1'] ? '0' : '1');
 
-  const minByDigits = maxByDigits
-    .map(c => c === '0' ? '1' : '0');
+  return maxByDigits;
+};
+
+const inverseBit = c => c === '0' ? '1' : '0';
+
+const solve1 = (string) => {
+  const numbers = string.trim().split('\n');
+
+  const maxByDigits = findMostCommonDigits(numbers);
+  const minByDigits = maxByDigits.map(inverseBit);
 
   const gammaRate = parseInt(maxByDigits.join(''), 2);
   const epsilonRate = parseInt(minByDigits.join(''), 2);
-
   const result = gammaRate * epsilonRate;
 
   return result;
 };
 
-const solve2 = () => {};
+const solve2 = (string) => {
+  const numbers = string.trim().split('\n');
+
+  const groups = numbers.reduce((groups, number) => {
+    number.split('').forEach((digit, i) => {
+      if(!groups[i]) groups[i] = [];
+
+      groups[i].push(digit);
+    });
+
+    return groups;
+  }, []);
+};
 
 export { solve1, solve2, readInput };
