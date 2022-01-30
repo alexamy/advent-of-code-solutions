50243e2d15989a863d6ec226d2dcc8370df6e257
Alexander Chaschin <ac@happynumbers.com>
2022-01-30 13:51:58 +0300

    fix typo

--- packages/sonar-sweep/index.test.js
+++ packages/sonar-sweep/index.test.js
@@ -4,6 +4,6 @@ it('returns right answer for part 1', () => {
   expect(solvePart1()).toBe(1532);
 });
 
-it('returns right answer for part 1', () => {
+it('returns right answer for part 2', () => {
   expect(solvePart2()).toBe(1571);
 });
