c781c509dc04af398ef9441882bc8a70724f068a
Alexander Chaschin <ac@happynumbers.com>
2022-01-30 17:28:27 +0300

    add solution for test case

--- packages/dive/index.js
+++ packages/dive/index.js
@@ -1,3 +1,19 @@
-const solve = () => {};
+const solve = (paths) => {
+  const lines = paths.trim().split('\n');
+  const instructions = lines
+    .map(line => line.split(' '))
+    .map(([dir, n]) => ([dir, Number(n)]));
+
+  const coordinates = instructions.reduce((acc, [dir, n]) => {
+    if(dir === 'forward') acc.h += n;
+    if(dir === 'down') acc.v += n;
+    if(dir === 'up') acc.v -= n;
+    return acc;
+  }, { h: 0, v: 0 });
+
+  const result = coordinates.h * coordinates.v;
+
+  return result;
+};
 
 export default solve;
--- packages/dive/index.test.js
+++ packages/dive/index.test.js
@@ -1,12 +1,13 @@
 import solve from './index.js';
 
-const route = `
+it('calculates test route', () => {
+  const route = `
 forward 5
 down 5
 forward 8
 up 3
 down 8
-forward 2
-`;
+forward 2`;
 
-expect(solve(route)).toBe(150);
+  expect(solve(route)).toBe(150);
+});
