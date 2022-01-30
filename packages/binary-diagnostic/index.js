a35f0e7d265a24a9b8572893d9138607bc784ab1
Alexander Chaschin <ac@happynumbers.com>
2022-01-30 20:47:41 +0300

    add part 2 solution

--- packages/binary-diagnostic/index.js
+++ packages/binary-diagnostic/index.js
@@ -7,7 +7,7 @@ const readInput = async () => {
   return input;
 };
 
-const findMostCommonDigits = (numbers) => {
+const findMostCommonDigits = (numbers, comparator) => {
   const groups = numbers.reduce((groups, number) => {
     number.split('').forEach((digit, i) => {
       if(!groups[i]) groups[i] = [];
@@ -20,7 +20,7 @@ const findMostCommonDigits = (numbers) => {
 
   const maxByDigits = groups
     .map(group => _.countBy(group, _.identity))
-    .map(c => c['0'] > c['1'] ? '0' : '1');
+    .map(comparator);
 
   return maxByDigits;
 };
@@ -30,7 +30,8 @@ const inverseBit = c => c === '0' ? '1' : '0';
 const solve1 = (string) => {
   const numbers = string.trim().split('\n');
 
-  const maxByDigits = findMostCommonDigits(numbers);
+  const comparator = c => c['1'] >= c['0'] ? '1' : '0';
+  const maxByDigits = findMostCommonDigits(numbers, comparator);
   const minByDigits = maxByDigits.map(inverseBit);
 
   const gammaRate = parseInt(maxByDigits.join(''), 2);
@@ -43,15 +44,31 @@ const solve1 = (string) => {
 const solve2 = (string) => {
   const numbers = string.trim().split('\n');
 
-  const groups = numbers.reduce((groups, number) => {
-    number.split('').forEach((digit, i) => {
-      if(!groups[i]) groups[i] = [];
+  let i = 0;
+  let generator = [...numbers];
+  while(generator.length > 1) {
+    const comparator = c => c['1'] >= c['0'] ? '1' : '0';
+    const maxDigit = findMostCommonDigits(generator, comparator)[i];
+    generator = generator.filter(digits => digits[i] === maxDigit);
+    i++;
+  }
 
-      groups[i].push(digit);
-    });
+  i = 0;
+  let scrubber = [...numbers];
+  while(scrubber.length > 1) {
+    const comparator = c => c['0'] <= c['1'] ? '0' : '1';
+    const minDigit = findMostCommonDigits(scrubber, comparator)[i];
+    scrubber = scrubber.filter(digits => digits[i] === minDigit);
+    i++;
+  }
 
-    return groups;
-  }, []);
+  generator = parseInt(generator.join(''), 2);
+  scrubber = parseInt(scrubber.join(''), 2);
+  console.log(generator, scrubber);
+
+  const result = generator * scrubber;
+
+  return result;
 };
 
 export { solve1, solve2, readInput };
