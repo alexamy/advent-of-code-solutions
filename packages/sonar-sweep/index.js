93ab82c0b83ab11906cd0485845149dbb6907975
Alexander Chaschin <ac@happynumbers.com>
2022-01-30 13:29:18 +0300

    add second solution

--- packages/sonar-sweep/index.js
+++ packages/sonar-sweep/index.js
@@ -16,6 +16,24 @@ for(let i = 0; i < measurements.length - 1; i++) {
   trendsByOne.push(next > current ? INC : DEC);
 }
 
+// count directions (part 2)
+const sumsByThree = [];
+for(let i = 0; i < measurements.length - 3; i++) {
+  const [w1, w2, w3] = measurements.slice(i, i + 3);
+  sumsByThree.push(w1 + w2 + w3);
+}
+
+const trendsByThree = [];
+for(let i = 0; i < sumsByThree.length - 1; i++) {
+  const current = sumsByThree[i];
+  const next = sumsByThree[i+1];
+
+  trendsByThree.push(next > current ? INC : DEC);
+}
+
 // find result count
 const resultPart1 = trendsByOne.filter(direction => direction === INC).length;
 console.log(`Part 1: ${resultPart1}`);
+
+const resultPart2 = trendsByThree.filter(direction => direction === INC).length;
+console.log(`Part 2: ${resultPart2}`);
