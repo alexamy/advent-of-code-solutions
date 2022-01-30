65f65ba1c4e64832b7b2c9b68081ef4e4b8a7635
Alexander Chaschin <ac@happynumbers.com>
2022-01-30 09:33:21 +0300

    split part1 in result

--- packages/sonar-sweep/index.js
+++ packages/sonar-sweep/index.js
@@ -7,15 +7,15 @@ const DEC = 'dec';
 const measurementsBuffer = await fs.promises.readFile('input.txt');
 const measurements = measurementsBuffer.toString().split('\n').map(Number);
 
-// count directions
-const trends = [];
+// count directions (part 1)
+const trendsByOne = [];
 for(let i = 0; i < measurements.length - 1; i++) {
   const current = measurements[i];
   const next = measurements[i+1];
 
-  trends.push(next > current ? INC : DEC);
+  trendsByOne.push(next > current ? INC : DEC);
 }
 
 // find result count
-const result = trends.filter(direction => direction === INC).length;
-console.log(result);
+const resultPart1 = trendsByOne.filter(direction => direction === INC).length;
+console.log(`Part 1: ${resultPart1}`);
