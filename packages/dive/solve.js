b9e0902172ae0d01aaf97c97e22527044e0d0082
Alexander Chaschin <ac@happynumbers.com>
2022-01-30 17:48:21 +0300

    merge solvers

--- packages/dive/solve.js
+++ packages/dive/solve.js
@@ -1,37 +1,31 @@
-const solve1 = (paths) => {
+const simpleNavigator = (acc, [dir, n]) => {
+  if(dir === 'forward') acc.h += n;
+  if(dir === 'down') acc.d += n;
+  if(dir === 'up') acc.d -= n;
+  return acc;
+}
+
+const complexNavigator = (acc, [dir, n]) => {
+  if(dir === 'forward') { acc.h += n; acc.d += n * acc.a; }
+  if(dir === 'down') acc.a += n;
+  if(dir === 'up') acc.a -= n;
+  return acc;
+}
+
+const solver = (startPosition, navigator) => (paths) => {
   const lines = paths.trim().split('\n');
   const instructions = lines
     .map(line => line.split(' '))
     .map(([dir, n]) => ([dir, Number(n)]));
 
-  const coordinates = instructions.reduce((acc, [dir, n]) => {
-    if(dir === 'forward') acc.h += n;
-    if(dir === 'down') acc.v += n;
-    if(dir === 'up') acc.v -= n;
-    return acc;
-  }, { h: 0, v: 0 });
-
-  const result = coordinates.h * coordinates.v;
-
-  return result;
-};
-
-const solve2 = (paths) => {
-  const lines = paths.trim().split('\n');
-  const instructions = lines
-    .map(line => line.split(' '))
-    .map(([dir, n]) => ([dir, Number(n)]));
-
-  const coordinates = instructions.reduce((acc, [dir, n]) => {
-    if(dir === 'forward') { acc.h += n; acc.d += n * acc.a; }
-    if(dir === 'down') acc.a += n;
-    if(dir === 'up') acc.a -= n;
-    return acc;
-  }, { h: 0, a: 0, d: 0 });
+  const coordinates = instructions.reduce(navigator, startPosition);
 
   const result = coordinates.h * coordinates.d;
 
   return result;
 };
+
+const solve1 = solver({ h: 0, d: 0 }, simpleNavigator);
+const solve2 = solver({ h: 0, d: 0, a: 0 }, complexNavigator);
 
 export { solve1, solve2 };
