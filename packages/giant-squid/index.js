da25112ee9090cefb231f5c9f0c2dd3e6cbea54c
Alexander Chaschin <ac@happynumbers.com>
2022-01-31 23:40:37 +0300

    add read, split, make boards

--- packages/giant-squid/index.js
+++ packages/giant-squid/index.js
@@ -1,4 +1,30 @@
-const solve1 = () => {};
+const SIZE = 5;
+
+const solve1 = (data) => {
+  // split data
+  const lines = data.trim().split('\n');
+
+  const drawn = lines[0];
+  let merged = lines.slice(2);
+
+  const boardsRaw = [];
+  while(merged.length) {
+    boardsRaw.push(merged.splice(0, SIZE));
+    merged.splice(0, 1);
+  }
+
+  // make boards
+  const boards = boardsRaw.map(board => {
+    return board.flatMap(row => row
+      .trim()
+      .replace(/\s+/g, ' ')
+      .split(' ')
+      .map(n => ({ mark: false, n: Number(n) }))
+    );
+  });
+
+  return boards;
+};
 
 const solve2 = () => {};
 
