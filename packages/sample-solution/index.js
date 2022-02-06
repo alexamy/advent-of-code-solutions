f4ca6a63a47afcf08e5abfeaaaea2ab50d2d88a4
Alexander Chaschin <ac@happynumbers.com>
2022-02-06 15:29:53 +0300

    refactor: add sample solution ts

--- packages/sample-solution/index.js
+++
@@ -1,5 +0,0 @@
-const solve1 = () => {};
-
-const solve2 = () => {};
-
-export { solve1, solve2 };
--- packages/sample-solution/index.test.ts
+++ packages/sample-solution/index.test.ts
@@ -1,9 +1,9 @@
-import { solve1, solve2 } from './index.js';
+import { solve1, solve2 } from './index';
 
 it.todo('calculates result for part 1', () => {
-  expect(solve1()).toBe();
+  expect(solve1('')).toBe({});
 });
 
 it.skip('calculates result for part 2', () => {
-  expect(solve2()).toBe();
+  expect(solve2('')).toBe({});
 });
