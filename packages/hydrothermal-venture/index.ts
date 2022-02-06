01be5d3c71b38243ac3080f0074b10b435905c37
Alexander Chaschin <ac@happynumbers.com>
2022-02-07 01:30:38 +0300

    feat: add split input data

--- packages/hydrothermal-venture/index.ts
+++ packages/hydrothermal-venture/index.ts
@@ -1,5 +1,17 @@
-const solve1 = (data: string) => {};
+type Solver = (data: string) => number;
 
-const solve2 = (data: string) => {};
+const solve1: Solver = (data) => {
+  const pairs = data.trim().split('\n')
+    .map(row => row.split(' -> '))
+    .map(pair => pair.map(coords => coords.split(',').map(Number)));
+
+
+
+  return pairs;
+};
+
+const solve2: Solver = (data) => {
+  return 0;
+};
 
 export { solve1, solve2 };
