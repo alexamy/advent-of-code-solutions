24f82ab160b863328d4409fdaf1c4457e38ef51d
Alexander Chaschin <ac@happynumbers.com>
2022-02-06 15:39:11 +0300

    fix(giant-squid): use proper module system

--- packages/giant-squid/run.ts
+++ packages/giant-squid/run.ts
@@ -1,8 +1,13 @@
-import { solve1, solve2, readInput } from './index.js';
+import fs from 'fs';
+import { solve1, solve2, readInput } from './index';
 
-const input = (await readInput()).toString();
-const result1 = solve1(input);
-const result2 = solve2(input);
+const solve = async () => {
+  const input = (await readInput()).toString();
+  const result1 = solve1(input);
+  const result2 = solve2(input);
 
-const messages = [`Part 1: ${result1}`, `Part 2: ${result2}`];
-console.log(messages.join('\n'));
+  const messages = [`Part 1: ${result1}`, `Part 2: ${result2}`];
+  console.log(messages.join('\n'));
+}
+
+solve();
