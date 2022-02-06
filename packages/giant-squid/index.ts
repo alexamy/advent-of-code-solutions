ec513bbb63899e94cbaf73f9f44386babdd42829
Alexander Chaschin <ac@happynumbers.com>
2022-02-06 13:39:59 +0300

    chore: add types

--- packages/giant-squid/index.ts
+++ packages/giant-squid/index.ts
@@ -1,8 +1,24 @@
 import fs from 'fs';
 import _ from 'lodash';
 
+// types
+interface Cell {
+  n: number;
+  mark: boolean;
+}
+
+type Board = Cell[];
+
+interface SolverContext {
+  number: number;
+  board: Board;
+  boards: Board[];
+}
+
+type Solver = (size: number) => (acc: SolverContext, number: number) => boolean;
+
 // constants
-const getWinRows = _.memoize(size => {
+const getWinRows = _.memoize((size: number): number[][] => {
   const getRow = i => _.range(i * size, i * size + size);
   const getColumn = i => _.range(i, i + size * size, size);
 
@@ -13,11 +29,13 @@ const getWinRows = _.memoize(size => {
 });
 
 // board helpers
-const parseBoard = board => board.flatMap(row => row.trim().replace(/\s+/g, ' ').split(' '));
+const parseBoard = (board: string[]): string[] =>
+  board.flatMap(row => row.trim().replace(/\s+/g, ' ').split(' '));
 
-const makeMarkedBoard = board => board.map(n => ({ mark: false, n: Number(n) }));
+const makeMarkedBoard = (board: string[]): Board =>
+  board.map(n => ({ mark: false, n: Number(n) }));
 
-const parseInputData = size => data => {
+const parseInputData = (size: number) => (data: string): { numbers: number[], boards: string[][] } => {
   const [numSeq, _delim, ...boardsRaw] = data.trim().split('\n');
   const numbers = numSeq.split(',').map(Number);
 
@@ -31,13 +49,13 @@ const parseInputData = size => data => {
   return { numbers, boards };
 }
 
-const markNumber = number => board => board.map(cell =>
-  cell.n === number ? ({ ...cell, mark: true }) : cell);
+const markNumber = (number: number) => (board: Board) =>
+  board.map(cell => cell.n === number ? ({ ...cell, mark: true }) : cell);
 
-const isWinBoard = size => board => getWinRows(size)
-  .some(idxs => idxs.every(idx => board[idx].mark));
+const isWinBoard = (size: number) => (board: Board): boolean =>
+  getWinRows(size).some(idxs => idxs.every(idx => board[idx].mark));
 
-const getScore = board => board
+const getScore = (board: Board) => board
   .filter(field => !field.mark)
   .map(field => field.n)
   .reduce((a, b) => a + b, 0);
@@ -45,7 +63,7 @@ const getScore = board => board
 // solvers
 const readInput = async () => await fs.promises.readFile('./input.txt');
 
-const solverFirst = size => (acc, number) => {
+const solverFirst: Solver = (size) => (acc, number) => {
   acc.number = number;
   acc.boards = acc.boards.map(markNumber(number));
   acc.board = acc.boards.find(isWinBoard(size));
@@ -53,7 +71,7 @@ const solverFirst = size => (acc, number) => {
   return !acc.board;
 }
 
-const solverLast = size => (acc, number) => {
+const solverLast: Solver = (size) => (acc, number) => {
   acc.number = number;
   acc.boards = acc.boards.map(markNumber(number));
   [[acc.board], acc.boards] = _.partition(acc.boards, isWinBoard(size));
@@ -61,13 +79,13 @@ const solverLast = size => (acc, number) => {
   return acc.boards.length > 0;
 }
 
-const solve = solver => data => {
+const solve = (solver: Solver) => (data: string) => {
   const size = 5;
   const { numbers, boards: boardsRaw } = parseInputData(size)(data);
   const boards = boardsRaw.map(makeMarkedBoard);
 
   return _(numbers)
-    .transform(solver(size), { boards })
+    .transform(solver(size), { boards, board: [], number: -1 })
     .thru(({ number, board }) => number * getScore(board))
     .value();
 };
