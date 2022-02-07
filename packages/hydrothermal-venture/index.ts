9b8ec79de27984206901a7aa3dcc30ba6dca364d
Alexander Chaschin <ac@happynumbers.com>
2022-02-07 09:26:12 +0300

    Revert "refactor: split horizontal / vertical paths"
    This reverts commit 82d4e4a6cb754d4e2fde587ce7e9efc5d0c4a723.
    
    And commit 58a8f13.

--- packages/hydrothermal-venture/index.ts
+++ packages/hydrothermal-venture/index.ts
@@ -5,33 +5,35 @@ type Solver = (data: string) => number;
 
 type Point = number[];
 
-type PathMaker = ([start, end]: Point[]) => Point[];
-
-const makeHorizontalPath: PathMaker = ([start, end]) => {
+const makePath = (start: Point, end: Point): Point[] => {
   const [x1, y1] = start;
-  const [x2, _y2] = end;
+  const [x2, y2] = end;
 
-  const xmin = Math.min(x1, x2);
-  const xmax = Math.max(x1, x2);
+  const x = x1 === x2 ? x1 : null;
+  const y = y1 === y2 ? y1 : null;
 
-  return _.range(xmin, xmax + 1).map(x => [x,y1]);
-}
+  if(x) {
+    const ymin = Math.min(y1, y2);
+    const ymax = Math.max(y1, y2);
 
-const makeVerticalPath: PathMaker = ([start, end]) => {
-  const [x1, y1] = start;
-  const [_x2, y2] = end;
+    return _.range(ymin, ymax + 1).map(y => [x,y]);
+  }
 
-  const ymin = Math.min(y1, y2);
-  const ymax = Math.max(y1, y2);
+  if(y) {
+    const xmin = Math.min(x1, x2);
+    const xmax = Math.max(x1, x2);
 
-  return _.range(ymin, ymax + 1).map(y => [x1,y]);
+    return _.range(xmin, xmax + 1).map(x => [x,y]);
+  }
+
+  return [[0,0]];
 }
 
 export const readInput = async () => (await fs.promises.readFile('./input.txt')).toString();
 
 export const solve1: Solver = (data) => {
-  const isVertical = ([start, end]: Point[]) => start[0] === end[0];
-  const isHorizontal = ([start, end]: Point[]) => start[1] === end[1];
+  const isHorizontal = (start: number[], end: number[]) => start[0] === end[0];
+  const isVertical = (start: number[], end: number[]) => start[1] === end[1];
 
   const pairs = data
     .trim()
@@ -39,15 +41,9 @@ export const solve1: Solver = (data) => {
     .map(row => row.split(' -> '))
     .map(pair => pair.map(coords => coords.split(',').map(Number)));
 
-  const horizontalPathData = pairs
-    .filter(isHorizontal)
-    .flatMap(makeHorizontalPath);
-
-  const verticalPathData = pairs
-    .filter(isVertical)
-    .flatMap(makeVerticalPath);
-
-  const pathData = horizontalPathData.concat(verticalPathData).flat(0);
+  const pathData = pairs
+    .filter(([start, end]) => isHorizontal(start, end) || isVertical(start, end))
+    .flatMap(([start, end]) => makePath(start, end));
 
   const count = _(pathData)
     .countBy()
