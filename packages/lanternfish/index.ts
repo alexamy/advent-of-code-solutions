a35fdcaabf373b6f6131e76acefc94c684e3f446
Alexander Chaschin <ac@happynumbers.com>
2022-02-07 21:31:10 +0300

    refactor: use lodash chain

--- packages/lanternfish/index.ts
+++ packages/lanternfish/index.ts
@@ -3,15 +3,14 @@ import _ from 'lodash';
 
 export const readInput = async () => (await fs.promises.readFile('./input.txt')).toString();
 
-export const solve1 = (data: string, day: number) => {
-  const daysInitial = data.trim().split(',').map(Number);
-
-  const { days } = _.transform(_.range(day), (acc) => {
-    acc.day += 1;
-    acc.days = acc.days.flatMap(n => n === 0 ? [6,8] : [n - 1]);
-  }, { day: 0, days: daysInitial });
-
-  return days.length;
-};
+export const solve1 = (data: string, day: number) => _
+  .chain(null)
+  .range(day)
+  .transform((acc) => {
+    acc.days = acc.days.flatMap(n => n === 0 ? [6,8] : [n-1]);
+  }, { days: data.trim().split(',').map(Number) })
+  .get('days')
+  .value()
+  .length;
 
 export const solve2 = (data: string, day: number) => {};
