38296282cac7ba47211302e14b0d495b83565aaa
Alexander Chaschin <ac@happynumbers.com>
2022-02-07 21:37:33 +0300

    refactor: use range + reduce only

--- packages/lanternfish/index.ts
+++ packages/lanternfish/index.ts
@@ -4,12 +4,10 @@ import _ from 'lodash';
 export const readInput = async () => (await fs.promises.readFile('./input.txt')).toString();
 
 export const solve1 = (initial: string, day: number) => _
-  .chain(null)
   .range(day)
   .reduce((days) =>
     days.flatMap(n => n === 0 ? [6,8] : [n-1]),
     initial.trim().split(',').map(Number))
-  .value()
   .length;
 
 export const solve2 = (data: string, day: number) => {};
