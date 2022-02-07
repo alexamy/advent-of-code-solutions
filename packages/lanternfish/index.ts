cd98f6a74bbb48a6a48ca3b4f48254a0a3e2803d
Alexander Chaschin <ac@happynumbers.com>
2022-02-07 21:33:29 +0300

    refactor: rename variable

--- packages/lanternfish/index.ts
+++ packages/lanternfish/index.ts
@@ -3,12 +3,12 @@ import _ from 'lodash';
 
 export const readInput = async () => (await fs.promises.readFile('./input.txt')).toString();
 
-export const solve1 = (data: string, day: number) => _
+export const solve1 = (initial: string, day: number) => _
   .chain(null)
   .range(day)
   .reduce((days) =>
     days.flatMap(n => n === 0 ? [6,8] : [n-1]),
-    data.trim().split(',').map(Number))
+    initial.trim().split(',').map(Number))
   .value()
   .length;
 
