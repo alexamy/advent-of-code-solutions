b2b192dca8e6927afe99fecfa3717adeb588dbeb
Alexander Chaschin <ac@happynumbers.com>
2022-02-07 11:12:31 +0300

    Refactor: hydrothermal venture (#14)

--- packages/hydrothermal-venture/index.ts
+++ packages/hydrothermal-venture/index.ts
@@ -1,79 +1,66 @@
 import fs from 'fs';
 import _ from 'lodash';
 
+type SolverGeneric = (data: string, filterF: (p: Point[]) => boolean) => number;
 type Solver = (data: string) => number;
 
-type Point = number[];
+interface Point {
+  x: number;
+  y: number;
+}
 
-const makePath = (start: Point, end: Point): Point[] => {
-  const [x1, y1] = start;
-  const [x2, y2] = end;
+const makePoint = ([x,y]: number[]) => ({ x, y });
 
-  const dx = Math.sign(x2 - x1);
-  const dy = Math.sign(y2 - y1);
+const makePath = ([start, end]: Point[]): Point[] => {
+  const { x: x1, y: y1 } = start;
+  const { x: x2, y: y2 } = end;
 
-  const horizontalRange = _.range(x1, x2 + dx, dx);
-  const verticalRange = _.range(y1, y2 + dy, dy);
+  const dx = Math.abs(x2 - x1);
+  const sx = Math.sign(x2 - x1);
 
-  const horizontal = horizontalRange.length > 0
-    ? horizontalRange
-    : _.times(verticalRange.length, _.constant(x1));
+  const dy = Math.abs(y2 - y1);
+  const sy = Math.sign(y2 - y1);
 
-  const vertical = verticalRange.length > 0
-    ? verticalRange
-    : _.times(horizontalRange.length, _.constant(y1));
+  const horizontal = dx === 0
+    ? _.times(dy + 1, _.constant(x1))
+    : _.range(x1, x2 + sx, sx);
 
-  return _.zip(horizontal, vertical) as Point[];
+  const vertical = dy === 0
+    ? _.times(dx + 1, _.constant(y1))
+    : _.range(y1, y2 + sy, sy);
+
+  // @ts-ignore
+  const points = _.zip(horizontal, vertical).map(makePoint)
+
+  return points as Point[];
 }
 
 export const readInput = async () => (await fs.promises.readFile('./input.txt')).toString();
 
-export const solve1: Solver = (data) => {
-  const isHorizontal = (start: number[], end: number[]) => start[0] === end[0];
-  const isVertical = (start: number[], end: number[]) => start[1] === end[1];
+const isHorizontal = ([start, end]: Point[]) =>
+  start.x === end.x;
 
-  const pairs = data
-    .trim()
-    .split('\n')
-    .map(row => row.split(' -> '))
-    .map(pair => pair.map(coords => coords.split(',').map(Number)));
+const isVertical = ([start, end]: Point[]) =>
+  start.y === end.y;
 
-  const pathData = pairs
-    .filter(([start, end]) => isHorizontal(start, end) || isVertical(start, end))
-    .flatMap(([start, end]) => makePath(start, end));
+const isDiagonal = ([start, end]: Point[]) =>
+  Math.abs(start.x - end.x) === Math.abs(start.y - end.y);
 
-  const count = _(pathData)
-    .countBy()
-    .values()
-    .filter(count => count > 1)
-    .value()
-    .length;
+export const solve: SolverGeneric = (data, filterF) =>
+  _(data)
+  .thru(s => s.trim().split('\n'))
+  .map(row => row.split(' -> ').map(coords => coords.split(',').map(Number)))
+  .map(([start, end]) => [makePoint(start), makePoint(end)])
+  .filter(filterF)
+  .flatMap(makePath)
+  .countBy(_.toPairs)
+  .values()
+  .filter(count => count > 1)
+  .value()
+  .length;
 
-  return count;
-};
+export const solve1: Solver = (data) =>
+  solve(data, _.overSome(isHorizontal, isVertical));
 
-export const solve2: Solver = (data) => {
-  const isHorizontal = (start: number[], end: number[]) => start[0] === end[0];
-  const isVertical = (start: number[], end: number[]) => start[1] === end[1];
-  const isDiagonal = (start: number[], end: number[]) =>
-    Math.abs(start[0] - end[0]) === Math.abs(start[1] - end[1]);
-
-  const pairs = data
-    .trim()
-    .split('\n')
-    .map(row => row.split(' -> '))
-    .map(pair => pair.map(coords => coords.split(',').map(Number)));
-
-  const pathData = pairs
-    .filter(([start, end]) => isHorizontal(start, end) || isVertical(start, end) || isDiagonal(start, end))
-    .flatMap(([start, end]) => makePath(start, end));
-
-  const count = _(pathData)
-    .countBy()
-    .values()
-    .filter(count => count > 1)
-    .value()
-    .length;
-
-  return count;
-};
+export const solve2: Solver = (data) =>
+  solve(data, _.overSome(isHorizontal, isVertical, isDiagonal));
