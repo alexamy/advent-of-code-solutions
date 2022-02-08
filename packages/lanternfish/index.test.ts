381d1b72c4afad8731a48d9c25a2e00cf6a4ef64
Alexander Chaschin <ac@happynumbers.com>
2022-02-08 11:31:33 +0300

    refactor: move time tests up

--- packages/lanternfish/index.test.ts
+++ packages/lanternfish/index.test.ts
@@ -2,20 +2,6 @@ import { readInput, solve } from './index';
 
 const data = `3,4,3,1,2`;
 
-it('calculates result for part 1 day 18', () => {
-  expect(solve(data, 18)).toBe(26);
-});
-
-it('calculates result for part 1 day 80', () => {
-  expect(solve(data, 80)).toBe(5934);
-});
-
-it('calculates result for part 1 file input', async () => {
-  const input = await readInput();
-
-  expect(solve(input, 80)).toBe(390011);
-});
-
 // it wont work just by bruteforce
 it('doesnt run too long', () => {
   const start = new Date();
@@ -32,6 +18,20 @@ it('doesnt run too long again', () => {
   const runTime = new Date().getTime() - start.getTime();
 
   expect(runTime).toBeLessThanOrEqual(500);
+});
+
+it('calculates result for part 1 day 18', () => {
+  expect(solve(data, 18)).toBe(26);
+});
+
+it('calculates result for part 1 day 80', () => {
+  expect(solve(data, 80)).toBe(5934);
+});
+
+it('calculates result for part 1 file input', async () => {
+  const input = await readInput();
+
+  expect(solve(input, 80)).toBe(390011);
 });
 
 it('calculates result for part 2', () => {
