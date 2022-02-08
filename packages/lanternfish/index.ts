a9311a7c55584d1ec245cd479f2014056f64aaaa
Alexander Chaschin <ac@happynumbers.com>
2022-02-08 08:11:56 +0300

    refactor: extract producer

--- packages/lanternfish/index.ts
+++ packages/lanternfish/index.ts
@@ -1,13 +1,13 @@
 import fs from 'fs';
 import _ from 'lodash';
 
+const producer = (n: number) => n === 0 ? [6,8] : [n-1];
+
 export const readInput = async () => (await fs.promises.readFile('./input.txt')).toString();
 
 export const solve1 = (initial: string, day: number) => _
   .range(day)
-  .reduce((days) =>
-    days.flatMap(n => n === 0 ? [6,8] : [n-1]),
-    initial.trim().split(',').map(Number))
+  .reduce((days) => days.flatMap(producer), initial.trim().split(',').map(Number))
   .length;
 
 export const solve2 = (data: string, day: number) => {
