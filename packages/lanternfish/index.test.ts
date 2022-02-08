d5f52c1be74299d33e8d167914651cc431f39c89
Alexander Chaschin <ac@happynumbers.com>
2022-02-08 12:16:01 +0300

    2021/6: Lanternfish (#15)

---
+++ packages/lanternfish/index.test.ts
@@ -0,0 +1,45 @@
+import { readInput, solve } from './index';
+
+const data = `3,4,3,1,2`;
+
+// it wont work just by bruteforce, `solve1` will fail
+it('doesnt run too long', () => {
+  const start = new Date();
+  solve(data, 130);
+  const runTime = new Date().getTime() - start.getTime();
+
+  expect(runTime).toBeLessThanOrEqual(500);
+});
+
+// it wont work just by bruteforce, `solve1` will fail
+it('doesnt run too long again', () => {
+  const start = new Date();
+  solve(data, 192);
+  const runTime = new Date().getTime() - start.getTime();
+
+  expect(runTime).toBeLessThanOrEqual(500);
+});
+
+it('calculates result for part 1 day 18', () => {
+  expect(solve(data, 18)).toBe(26);
+});
+
+it('calculates result for part 1 day 80', () => {
+  expect(solve(data, 80)).toBe(5934);
+});
+
+it('calculates result for part 1 file input', async () => {
+  const input = await readInput();
+
+  expect(solve(input, 80)).toBe(390011);
+});
+
+it('calculates result for part 2', () => {
+  expect(solve(data, 256)).toBe(26984457539);
+});
+
+it('calculates result for part 2 file input', async () => {
+  const input = await readInput();
+
+  expect(solve(input, 256)).toBe(1746710169834);
+});
---
+++ packages/lanternfish/index.ts
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import _ from 'lodash';
+
+const producer = (n: number) => n === 0 ? [6,8] : [n-1];
+
+export const readInput = async () => (await fs.promises.readFile('./input.txt')).toString();
+
+export const solve1 = (initial: string, day: number) => _
+  .range(day)
+  .reduce((days) => days.flatMap(producer), initial.trim().split(',').map(Number))
+  .length;
+
+const transformer = (next: number[]) => (count: number, day: number) => {
+  if(day === 0) {
+    next[6] = count;
+    next[8] = count;
+  }
+  else {
+    next[day-1] = (next[day-1] ?? 0) + count;
+  }
+}
+
+export const solve2 = (data: string, days: number) => {
+  const countsStart = data
+    .trim().split(',').map(Number)
+    .reduce((acc, k) => {
+      acc[k] = (acc[k] ?? 0) + 1;
+      return acc;
+    }, [] as number[]);
+
+  const counts = [...Array(days).keys()]
+    .reduce(counts => {
+      const next = Array(counts.length).fill(0);
+      counts.forEach(transformer(next));
+      return next;
+    }, countsStart);
+
+  const length = [...Array(counts.length).keys()]
+    .map(k => counts[k])
+    .reduce((a, b) => a + b);
+
+  return length;
+};
+
+export const solve = solve2;
