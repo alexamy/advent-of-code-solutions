4a9de6f5e6d3528f465a56272eae3be6fe6506da
Alexander Chaschin <ac@happynumbers.com>
2022-02-09 16:08:20 +0300

    Refactor: lanternfish (#16)
    Add lodash to solve2

--- packages/lanternfish/index.ts
+++ packages/lanternfish/index.ts
@@ -1,43 +1,47 @@
 import fs from 'fs';
 import _ from 'lodash';
 
-const producer = (n: number) => n === 0 ? [6,8] : [n-1];
-
 export const readInput = async () => (await fs.promises.readFile('./input.txt')).toString();
+
+const producer = (n: number) => n === 0 ? [6,8] : [n-1];
 
 export const solve1 = (initial: string, day: number) => _
   .range(day)
   .reduce((days) => days.flatMap(producer), initial.trim().split(',').map(Number))
   .length;
 
-const transformer = (next: number[]) => (count: number, day: number) => {
-  if(day === 0) {
-    next[6] = count;
-    next[8] = count;
-  }
-  else {
-    next[day-1] = (next[day-1] ?? 0) + count;
-  }
-}
+const safeSum = (a: number, b: number) => isFinite(a + b) ? (a + b) : undefined;
+const sumDays = (xs: Record<string, number>[]) => _.mergeWith({}, ...xs, safeSum)
+
+const dayInit = (k: string) => ({ [k]: 1 });
+const nextDay = (count: number, day: string): Record<string, number> =>
+  +day === 0
+    ? { 6: count, 8: count }
+    : { [+day-1]: count };
+
+const dayIteratee = (counts: Record<string, number>) =>
+  _(counts)
+  .mapValues(nextDay)
+  .values()
+  .thru(sumDays)
+  .value();
 
 export const solve2 = (data: string, days: number) => {
-  const countsStart = data
-    .trim().split(',').map(Number)
-    .reduce((acc, k) => {
-      acc[k] = (acc[k] ?? 0) + 1;
-      return acc;
-    }, [] as number[]);
+  const initDays: Record<string, number> =
+    _(data)
+    .thru(s => s.trim().split(',').map(dayInit))
+    .thru(sumDays)
+    .value();
 
-  const counts = [...Array(days).keys()]
-    .reduce(counts => {
-      const next = Array(counts.length).fill(0);
-      counts.forEach(transformer(next));
-      return next;
-    }, countsStart);
+  const count = _
+    .chain(days)
+    .range()
+    .reduce(dayIteratee, initDays)
+    .values()
+    .sum()
+    .value();
 
-  const length = counts.reduce((a, b) => a + b);
-
-  return length;
+  return count;
 };
 
 export const solve = solve2;
