69603a0d6dbeea0b609fd9817f2d7895e110a170
Alexander Chaschin <ac@happynumbers.com>
2022-02-09 19:23:13 +0300

    add part 1 solution

--- packages/the-treachery-of-whales/index.ts
+++ packages/the-treachery-of-whales/index.ts
@@ -1,3 +1,14 @@
-export const solve1 = (data: string) => {};
+export const solve1 = (data: string) => {
+  const positions = data.trim().split(',').map(Number);
+  positions.sort((a, b) => a - b);
+
+  const median = positions[positions.length/2];
+
+  const fuel = positions
+    .map(pos => Math.abs(pos - median))
+    .reduce((a, b) => a + b);
+
+  return fuel;
+};
 
 export const solve2 = (data: string) => {};
