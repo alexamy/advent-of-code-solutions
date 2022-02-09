8d772d4723f3ddc68de850987229cfed072ea3e6
Alexander Chaschin <ac@happynumbers.com>
2022-02-09 18:59:26 +0300

    add task info

--- packages/the-treachery-of-whales/index.test.ts
+++ packages/the-treachery-of-whales/index.test.ts
@@ -1,24 +1,18 @@
-import { readInput, solve1, solve2 } from './index';
+import { fileData, testData } from './data';
+import { solve1, solve2 } from './index';
 
-const data = `
-`;
-
-it.todo('calculates result for part 1', () => {
-  expect(solve1(data)).toBe({});
+it.todo('calculates result for part 1 test data', () => {
+  expect(solve1(testData)).toBe(37);
 });
 
-it.skip('calculates result for part 1 file input', async () => {
-  const input = await readInput();
-
-  expect(solve1(input)).toBe({});
+it.skip('calculates result for part 1', async () => {
+  expect(solve1(fileData)).toBe({});
 });
 
-it.skip('calculates result for part 2', () => {
-  expect(solve2(data)).toBe({});
+it.skip('calculates result for part 2 test data', () => {
+  expect(solve2(testData)).toBe({});
 });
 
-it.skip('calculates result for part 2 file input', async () => {
-  const input = await readInput();
-
-  expect(solve2(input)).toBe({});
+it.skip('calculates result for part 2', async () => {
+  expect(solve2(fileData)).toBe({});
 });
--- packages/the-treachery-of-whales/index.ts
+++ packages/the-treachery-of-whales/index.ts
@@ -1,7 +1,3 @@
-import fs from 'fs';
-
-export const readInput = async () => (await fs.promises.readFile('./input.txt')).toString();
-
 export const solve1 = (data: string) => {};
 
 export const solve2 = (data: string) => {};
