9889e03e21d99da310eecc1c3cc0f29a1b7fea77
Alexander Chaschin <ac@happynumbers.com>
2022-02-09 19:37:13 +0300

    remove async from test

--- packages/the-treachery-of-whales/index.test.ts
+++ packages/the-treachery-of-whales/index.test.ts
@@ -1,11 +1,11 @@
-import { fileData, testData } from './data';
 import { solve1, solve2 } from './index';
+import { fileData, testData } from './data';
 
 it('calculates result for part 1 test data', () => {
   expect(solve1(testData)).toBe(37);
 });
 
-it('calculates result for part 1', async () => {
+it('calculates result for part 1', () => {
   expect(solve1(fileData)).toBe(352997);
 });
 
@@ -13,6 +13,6 @@ it('calculates result for part 2 test data', () => {
   expect(solve2(testData)).toBe(168);
 });
 
-it.skip('calculates result for part 2', async () => {
+it.skip('calculates result for part 2', () => {
   expect(solve2(fileData)).toBe({});
 });
