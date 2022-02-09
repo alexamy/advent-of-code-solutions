358d0cc10aaa2e6512c33259de22856f1ea6e67c
Alexander Chaschin <ac@happynumbers.com>
2022-02-09 15:54:26 +0300

    reorder methods

--- packages/lanternfish/index.ts
+++ packages/lanternfish/index.ts
@@ -1,42 +1,42 @@
 import fs from 'fs';
 import _ from 'lodash';
 
-const producer = (n: number) => n === 0 ? [6,8] : [n-1];
-
 export const readInput = async () => (await fs.promises.readFile('./input.txt')).toString();
+
+const producer = (n: number) => n === 0 ? [6,8] : [n-1];
 
 export const solve1 = (initial: string, day: number) => _
   .range(day)
   .reduce((days) => days.flatMap(producer), initial.trim().split(',').map(Number))
   .length;
 
+const safeSum = (a: number, b: number) => isFinite(a + b) ? (a + b) : undefined;
+const sumDays = (xs: Record<string, number>[]) => _.mergeWith({}, ...xs, safeSum)
+
+const dayInit = (k: string) => ({ [k]: 1 });
+const nextDay = (count: number, day: string): Record<string, number> =>
+  +day === 0
+    ? { 6: count, 8: count }
+    : { [+day-1]: count };
+
 export const solve2 = (data: string, days: number) => {
-  const safeSum = (a: number, b: number) => isFinite(a + b) ? (a + b) : undefined;
-  const sumDays = (xs: Record<string, number>[]) => _.mergeWith({}, ...xs, safeSum)
-
-  const dayInit = (k: string) => ({ [k]: 1 });
-  const nextDay = (count: number, day: string): Record<string, number> =>
-    +day === 0
-      ? { 6: count, 8: count }
-      : { [+day-1]: count };
-
-  const start: Record<string, number> =
-    _(data)
-    .thru(s => s.trim().split(',').map(dayInit))
+  const dayIteratee = (counts: Record<string, number>) =>
+    _(counts)
+    .mapValues(nextDay)
+    .values()
     .thru(sumDays)
     .value();
 
-  const dayIteratee = (counts: Record<string, number>) =>
-    _(counts)
-    .mapValues<Record<string, number>>(nextDay)
-    .values()
+  const initDays: Record<string, number> =
+    _(data)
+    .thru(s => s.trim().split(',').map(dayInit))
     .thru(sumDays)
     .value();
 
   const count = _
     .chain(days)
     .range()
-    .reduce(dayIteratee, start)
+    .reduce(dayIteratee, initDays)
     .values()
     .sum()
     .value();
